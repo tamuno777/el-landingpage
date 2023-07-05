e9fc9df6b0b0fb4084f6bba699441d2c80fb6f0c
tamuno777 <davisbeulah07@gmail.com>
2023-07-05 12:19:23 -0700

    tired

--- src/component/Properties/properties.js
+++ src/component/Properties/properties.js
@@ -17,7 +17,7 @@ export default function Properties() {
            
            <Row className='mb-2' >
                 <Col className='H2'>
-                    <img src={H1} alt="image" 
+                    <motion.img src={H1} alt="image" 
                     
                     initial={{
                         x:-100,
@@ -30,7 +30,7 @@ export default function Properties() {
                     />
                 </Col>
                 <Col className='H3'>
-                    <img src={H2} alt="image"
+                    <motion.img src={H2} alt="image"
                      initial={{
                         x:100,
                     }}
@@ -44,7 +44,7 @@ export default function Properties() {
             </Row>
             <Row>
                 <Col className='H2'>
-                    <img src={H3} alt="image"
+                    <motion.img src={H3} alt="image"
                      initial={{
                         x:-100,
                     }}
@@ -56,7 +56,7 @@ export default function Properties() {
                    />
                 </Col>
                 <Col className='H3'>
-                    <img src={H4} alt="image" 
+                    <motion.img src={H4} alt="image" 
                      initial={{
                         x:100,
                     }}
