e878da66200aa139552f6af1d11d82df2a399713
tamuno777 <davisbeulah07@gmail.com>
2023-07-05 11:54:20 -0700

    oo

--- src/Nav/Navbar.js
+++ src/Nav/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from '../El img/EL.png'
+import logo from '../img/EL.png'
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
